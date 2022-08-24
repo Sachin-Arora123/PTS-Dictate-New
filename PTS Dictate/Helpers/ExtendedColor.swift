42ac194d46f78c6d60a295bc0100efca54f2886c
Sachin <abc@abcs-iMac.local>
2022-08-24 20:23:25 +0530

    first commit

---
+++ PTS Dictate/Helpers/ExtendedColor.swift
@@ -0,0 +1,16 @@
+//
+//  ExtendedColor.swift
+//  PTS Dictate
+//
+//  Created by Sachin on 23/08/22.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    class var appThemeColor : UIColor {
+        return UIColor(named: "AppThemeColor")!
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# PTS-Dictate-New
